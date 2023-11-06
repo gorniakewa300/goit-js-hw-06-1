9d06614204c743580d425bbbe10e30fc7aad1df7
gorniakewa300 <132227665+gorniakewa300@users.noreply.github.com>
2023-11-06 22:10:38 +0100

    exe 8-9

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,9 +1,21 @@
-const textInput = document.querySelector("#validation-input");
+
+//pierwszy sposób//
+/*const textInput = document.querySelector("#validation-input");
 const dataLength = textInput.getAttribute("data-length");
 
 textInput.addEventListener("blur", (event) => {
   if (event.currentTarget.value.length == dataLength) {
     textInput.classList.add("valid");
+  } else {
+    textInput.classList.add("invalid");
+  }
+});*/
+//drugi sposób//
+const textInput = document.querySelector("#validation-input");
+
+textInput.addEventListener("blur", () => {
+  if (textInput.value.length == textInput.getAttribute("data-length")) {
+      textInput.classList.add("valid");
   } else {
     textInput.classList.add("invalid");
   }
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorResult = document.querySelector(".color");
+
+btn.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorResult.textContent = color;
+});
