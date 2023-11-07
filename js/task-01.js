c36531c88a7eeef657da51b741e0b21d25581dcd
gorniakewa300 <132227665+gorniakewa300@users.noreply.github.com>
2023-11-07 11:33:57 +0100

    corrected task-1

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,6 @@ const allCategories = document.querySelectorAll(".item");
 console.log(`Number of categories:`, allCategories.length);
 
 allCategories.forEach((item) => {
-  console.log(`Category:`, item.firstElementChild);
+  console.log(`Category:`, item.firstElementChild.textContent);
   console.log((`Elements:`, item.lastElementChild.children.length));
 })
